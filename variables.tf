b8f1f9bb9bb4f708455be0b5f4033ef15d7fca74
Ganesh Balimidi <balimidi.ganesh@outlook.com>
2022-08-25 22:37:34 +0530

    update

--- variables.tf
+++ variables.tf
@@ -380,18 +380,10 @@ variable "virtual_machines" {
   default     = {}
   description = "Create a virtual machine from CAF module"
 }
-variable "resource_groups" {
-  default = {}
-}
+
 variable "vnets" {
   default = {}
 }
 variable "public_ip_addresses" {
-  default = {}
-}
-variable "keyvaults" {
-  default = {}
-}
-variable "global_settings" {
   default = {}
 }
