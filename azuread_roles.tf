d19b351802277967e185697af07e6a25a5ec6fa1
Ganesh Balimidi <balimidi.ganesh@outlook.com>
2022-08-25 23:33:58 +0530

    update

--- azuread_roles.tf
+++ azuread_roles.tf
@@ -23,34 +23,34 @@ module "azuread_roles_service_principals" {
   azuread_roles = each.value.roles
 }
 
-module "azuread_roles_msi" {
-  source   = "./modules/azuread/roles"
-  for_each = try(local.azuread.azuread_roles.managed_identities, {})
+# module "azuread_roles_msi" {
+#   source   = "./modules/azuread/roles"
+#   for_each = try(local.azuread.azuread_roles.managed_identities, {})
 
-  object_id     = module.managed_identities[each.key].principal_id
-  azuread_roles = each.value.roles
-}
+#   object_id     = module.managed_identities[each.key].principal_id
+#   azuread_roles = each.value.roles
+# }
 
-module "azuread_roles_sql_mi" {
-  source   = "./modules/azuread/roles"
-  for_each = try(local.azuread.azuread_roles.mssql_managed_instances, {})
+# module "azuread_roles_sql_mi" {
+#   source   = "./modules/azuread/roles"
+#   for_each = try(local.azuread.azuread_roles.mssql_managed_instances, {})
 
-  object_id     = module.mssql_managed_instances[each.key].principal_id
-  azuread_roles = each.value.roles
-}
+#   object_id     = module.mssql_managed_instances[each.key].principal_id
+#   azuread_roles = each.value.roles
+# }
 
-module "azuread_roles_sql_mi_secondary" {
-  source   = "./modules/azuread/roles"
-  for_each = try(local.azuread.azuread_roles.mssql_managed_instances_secondary, {})
+# module "azuread_roles_sql_mi_secondary" {
+#   source   = "./modules/azuread/roles"
+#   for_each = try(local.azuread.azuread_roles.mssql_managed_instances_secondary, {})
 
-  object_id     = module.mssql_managed_instances_secondary[each.key].principal_id
-  azuread_roles = each.value.roles
-}
+#   object_id     = module.mssql_managed_instances_secondary[each.key].principal_id
+#   azuread_roles = each.value.roles
+# }
 
-module "azuread_roles_mssql_server" {
-  source   = "./modules/azuread/roles"
-  for_each = try(local.azuread.azuread_roles.mssql_servers, {})
+# module "azuread_roles_mssql_server" {
+#   source   = "./modules/azuread/roles"
+#   for_each = try(local.azuread.azuread_roles.mssql_servers, {})
 
-  object_id     = module.mssql_servers[each.key].rbac_id
-  azuread_roles = each.value.roles
-}
+#   object_id     = module.mssql_servers[each.key].rbac_id
+#   azuread_roles = each.value.roles
+# }
